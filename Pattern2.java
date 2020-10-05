0adf241783c0a88ed6199f8c9382ecd50362a046
Pranay Kumar Reddy Goraka <63572862+Pranay2510@users.noreply.github.com>
2020-10-05 22:44:26 +0530

    Code 89 Day 36

---
+++ Pattern2.java
@@ -0,0 +1,12 @@
+package Patterns;
+public class Pattern2 {
+	public static void main(String[] args) {
+		for(int i =1; i<=5;i++) // rows
+		{
+			for(int j=1; j<=5;j++) // cols
+			{
+				System.out.print(i); // 11111
+			}
+			System.out.println();
+		}
+	} }
