4fc3adcb430ea6ff9859b854a89bfcc0fb52e9c8
Pranay Kumar Reddy Goraka <63572862+Pranay2510@users.noreply.github.com>
2020-10-07 22:06:37 +0530

    Code 95 Day 36

---
+++ Pattern8.java
@@ -0,0 +1,15 @@
+package Patterns;
+public class Pattern8 {
+	public static void main(String[] args) {
+		char i;
+		char j;
+		for( i ='E'; i>='A';i--) // rows
+		{
+			for( j='E'; j>='A';j--) // cols
+			{
+				System.out.print(i); //EEEEE
+			}
+			System.out.println();
+		}
+	} }
+
