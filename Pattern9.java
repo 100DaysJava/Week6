f56e0e76078dc69e19156f8281064bc377b766c5
Pranay Kumar Reddy Goraka <63572862+Pranay2510@users.noreply.github.com>
2020-10-07 22:07:14 +0530

    Code 96 Day 36

---
+++ Pattern9.java
@@ -0,0 +1,14 @@
+package Patterns;
+public class Pattern9 {
+	public static void main(String[] args) {
+		char i;
+		char j;
+		for( i ='E'; i>='A';i--) // rows
+		{
+			for( j='E'; j>='A';j--) // cols
+			{
+				System.out.print(J); //EDCBA
+			}
+			System.out.println();
+		}
+	} }
