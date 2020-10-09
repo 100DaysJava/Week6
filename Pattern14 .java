cba2064f0e5def7c4b6cfefd7c82f0b2741e68a1
Pranay Kumar Reddy Goraka <63572862+Pranay2510@users.noreply.github.com>
2020-10-09 22:39:11 +0530

    Code 101 Day 36

---
+++ Pattern14 .java
@@ -0,0 +1,15 @@
+package Patterns;
+public class Pattern14 {
+	public static void main(String[] args) {
+		char i;
+		char j;
+		for( i ='A'; i<='E';i++) //5 rows
+		{
+			for( j='A'; j<=i;j++) // cols
+			{
+				System.out.print(j); //A
+			}
+			System.out.println();
+		}
+	} }
+
