827c2c214bb61ef3cf228a22b29b668888a36f73
Pranay Kumar Reddy Goraka <63572862+Pranay2510@users.noreply.github.com>
2020-10-09 22:36:14 +0530

    Code 100 Day 36

---
+++ Pattern13.java
@@ -0,0 +1,15 @@
+package Patterns;
+public class Pattern13 {
+	public static void main(String[] args) {
+		char i;
+		char j;
+		for( i ='A'; i<='E';i++) // rows
+		{
+			for( j='A'; j<=i;j++) // cols
+			{
+				System.out.print(i); //A
+			}
+			System.out.println();
+		}
+	} }
+
