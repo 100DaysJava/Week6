8b2177e17a1e866fe22f00e0c69526a19ce3a46e
Pranay Kumar Reddy Goraka <63572862+Pranay2510@users.noreply.github.com>
2020-10-11 22:36:05 +0530

    Code 108 Day 38

---
+++ Pattern21.java
@@ -0,0 +1,12 @@
+package Patterns;
+public class Pattern21 {
+	public static void main(String[] args) {
+	for(int i= 1; i<=5;i++) // 5 rows
+	{
+		for(int j = 5  ; j >= i; j--)  // j=5,4,3,2,1
+		{
+			System.out.print(j+" "); // 5 
+		}
+		System.out.println();
+	}
+ } }
