27a065804d2f0273ff621cad507e5efaf03d9519
Pranay Kumar Reddy Goraka <63572862+Pranay2510@users.noreply.github.com>
2020-10-11 22:34:49 +0530

    Code 106 Day 38

---
+++ Pattern19.java
@@ -0,0 +1,12 @@
+Package Patterns;
+public class Pattern19 {
+	public static void main(String[] args) {
+	for(int i='E'; i>='A';i--) // 5 rows
+	{
+		for(int j= 'A';j<=i; j++) // 5,4 cols
+		{
+			System.out.print((char)j+" "); // A A A A A 
+		}
+		System.out.println(); //nextline
+	}
+} }
