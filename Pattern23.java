02b4ec99361b5a6d28999600723bf7ba3130e9d7
Pranay Kumar Reddy Goraka <63572862+Pranay2510@users.noreply.github.com>
2020-10-12 20:55:23 +0530

    Code 110 Day 38

---
+++ Pattern23.java
@@ -0,0 +1,13 @@
+package Patterns;
+public class Pattern23 {
+
+	public static void main(String[] args) {
+		for(int i='A'; i <= 'E';i++) // 5 rows
+		{
+			for(int j= 'E'; j >= i; j--) // 5,4 cols
+			{
+				System.out.print((char)j+" "); 
+			}
+			System.out.println(); //nextline
+		}
+} }
