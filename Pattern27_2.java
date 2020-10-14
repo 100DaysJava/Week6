69216eb35653bda3243fc4cfd52bbfde5d6d9e11
Pranay Kumar Reddy Goraka <63572862+Pranay2510@users.noreply.github.com>
2020-10-14 21:59:46 +0530

    Code 115 Day 39

---
+++ Pattern27_2.java
@@ -0,0 +1,16 @@
+package Patterns;
+public class Pattern27_2 {
+	public static void main(String[] args) {
+	int n =5;
+	
+	for(int i=1; i<=5; i++) // 5 rows
+	{
+		for(int j=n-1; j>=i; j--) // 4 spaces
+			System.out.print(" ");
+		
+		for(int k= 1; k<=i; k++) // repeats
+			System.out.print((char)(i+64)); // A
+		
+		System.out.println();
+	}
+} }
